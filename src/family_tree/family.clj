2788e9a7ca1f92dfe33fdd13bfdfcbfa8b0bc3b2
v-the-nomad <->
2012-06-06 00:15:54 +0200

    read families - it works

--- src/family_tree/family.clj
+++ src/family_tree/family.clj
@@ -1,16 +1,18 @@
-(defn read-families []
-  (let [line "<Bregor> parent_of <Hirwen>, <Gilwen>, <Bregil>, <Bregolas>, <Barahir>, <Gwindor>."
-        parent-children (re-find #"(<\w+>) parent_of (.+)" line)
-        child-matcher (re-matcher #"<(\w+)>" (last parent-children))
-        children (extract-children child-matcher)]
-    (println "+++ children:")
-    (println children)))
-
 (defn extract-children [to-extract]
   (let [child (re-find to-extract)]
     (if child
-      (do
-        (print "child:")
-        (println child)
-        (conj (extract-children to-extract) (last child)))
+      (conj (extract-children to-extract) (last child))
       (vector))))
+
+(defn read-a-family [families line]
+  (let [parent-children (re-find #"<(\w+)> parent_of (.+)" line)]
+    (if parent-children
+      (let [child-matcher (re-matcher #"<(\w+)>" (last parent-children))
+            children (extract-children child-matcher)
+            parent (second parent-children)]
+        (assoc families parent children))
+      families)))
+
+(defn read-families []
+  (with-open [rdr (clojure.java.io/reader "/Users/vschepik/src/clojure/family-tree/people.data")]
+    (reduce read-a-family (hash-map) (line-seq rdr))))
