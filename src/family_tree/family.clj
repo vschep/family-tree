ccfa63ac249fa992b20dff1ea045eb37bd515b59
v-the-nomad <->
2012-06-11 04:43:35 +0200

    read married-to

--- src/family_tree/family.clj
+++ src/family_tree/family.clj
@@ -4,7 +4,7 @@
       (conj (extract-children to-extract) (last child))
       (vector))))
 
-(defn read-a-family [families line]
+(defn read-parent-of [families line]
   (let [parent-children (re-find #"<(\w+)> parent_of (.+)" line)]
     (if parent-children
       (let [child-matcher (re-matcher #"<(\w+)>" (last parent-children))
@@ -13,6 +13,18 @@
         (assoc families parent children))
       families)))
 
+(defn read-married-to[couples line]
+  (let [married-to (re-find #"<(\w+)> married_to <(\w+)>" line)]
+    (if married-to
+      (store-couple couples (second married-to) (last married-to))
+      couples)))
+
+(defn store-couple [couples the-one the-other]
+  (assoc (assoc couples the-one the-other) the-other the-one))
+
+(defn read-a-family [[families couples] line]
+  [(read-parent-of families line) (read-married-to couples line)])
+
 (defn read-families []
   (with-open [rdr (clojure.java.io/reader "/Users/vschepik/src/clojure/family-tree/people.data")]
-    (reduce read-a-family (hash-map) (line-seq rdr))))
+    (reduce read-a-family [(hash-map) (hash-map)] (line-seq rdr))))
