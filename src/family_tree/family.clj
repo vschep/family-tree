09db6a16d344622b0d39171ed56164703ee48a11
v-the-nomad <->
2012-06-22 15:32:28 +0200

    get parent-of

--- src/family_tree/family.clj
+++ src/family_tree/family.clj
@@ -6,13 +6,15 @@
   [x]
   `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
+;;; read the file
+
 (defn extract-children [to-extract]
   (let [child (re-find to-extract)]
     (if child
       (conj (extract-children to-extract) (last child))
       (vector))))
 
-(defn ^:dynamic read-parent-of [families line]
+(defn read-parent-of [families line]
   (let [parent-children (re-find #"<(\w+)> parent_of (.+)" line)]
     (if parent-children
       (let [child-matcher (re-matcher #"<(\w+)>" (last parent-children))
@@ -38,5 +40,33 @@
     (dosync
       (ref-set fam-db (reduce read-a-family [(hash-map) (hash-map)] (line-seq rdr))))))
 
-(defn ^:dynamic children-of [parent]
+;;; query the family-tree
+
+(defn children-of [parent]
   ((first @fam-db) parent))
+
+;; TODO implement
+(defn root [fam-db]
+  "Gullin")
+
+(defn find-parent [{fam-db :fam-db 
+                              current-parent :current-parent 
+                              child :child}]
+  (let [children ((first fam-db) current-parent)]
+    (cond 
+      (not children) nil
+      (some (partial = child) children) current-parent
+      :default (let [max-children (count children)]
+                 (loop [n 0
+                        next-child (get children n)
+                        found-parent nil]
+                   (if (or (< n max-children) (not found-parent))
+                     (recur (+ n 1) (get children n) (find-parent {:fam-db fam-db :current-parent next-child :child child}))
+                     found-parent))))))
+
+
+(defn parent-of [child]
+  (find-parent {:fam-db @fam-db 
+                :current-parent (root @fam-db) 
+                :child child}))
+
--- test/family_tree/test/core.clj
+++ test/family_tree/test/core.clj
@@ -2,6 +2,8 @@
   (:use [family-tree.core])
   (:use [clojure.test]))
 
+;;; read the file
+
 (deftest test-read-parent-of-empty-hash-1
   (is (= (read-parent-of {} "<Bregor> parent_of <Hirwen>.") {"Bregor" ["Hirwen"]})))
 
@@ -20,10 +22,19 @@
 (deftest test-read-parent-of-no-match
   (is (read-parent-of {} "<Gilwen> married_to <Alatar>.") {}))
 
+
+;;; query the family-tree
+
 (deftest test-children-of
   (dosync (ref-set fam-db [{"Bregor" ["Hirwen" "Gilwen"]
                            "Gilwen" ["Hiril" "Hareth"]}]))
-    (let [children (children-of "Gilwen")]
-      (is (and 
-            (some (partial = "Hiril") children)
-            (some (partial = "Hareth") children)))))
+  (let [children (children-of "Gilwen")]
+    (is (and 
+          (some (partial = "Hiril") children)
+          (some (partial = "Hareth") children)))))
+
+(deftest test-parents-of-without-marriage
+  (dosync (ref-set fam-db [{"Gullin" ["Bregor" "Gwindor"]
+                           "Bregor" ["Bregil" "Arachon"]}]))
+  (is (= "Bregor" (parent-of "Bregil"))))
+
